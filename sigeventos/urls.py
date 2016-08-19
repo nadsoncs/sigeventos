c2afba3ba2b02e9a351dc347766cd6aeb211ef72
nadsoncs <nadsoncerqueira@gmail.com>
2016-08-19 10:02:53 -0300

    media update

--- sigeventos/urls.py
+++ sigeventos/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('eventos.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(
